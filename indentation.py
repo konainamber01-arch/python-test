a5699aa1bb6922594d8f2de3234f2ca8ca544b29
konainamber-01-arch <konainamber01@gmail.com>
2026-07-23 17:25:24 +0500

    added comment in python

--- indentation.py
+++ indentation.py
@@ -12,3 +12,22 @@ if 2 > 5:
 
 print("Main code chul raha hai")
 print("hello world")
+
+
+# True Condition Testing
+
+if 10 > 3:
+    print("10 is greater than 3")
+    print("i am python developer")
+
+print("my father name is muhammad yameen")
+
+# False Condition Testing 
+
+if 3 > 10:
+    print("3 is greater than 10")
+    print("my name is amber")
+    print("i love my self")
+
+print("python is easy language")
+print("who i am ")
