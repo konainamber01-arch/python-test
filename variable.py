45d6094434517dc16f4f2ba0267eea0d31be442f
konainamber-01-arch <konainamber01@gmail.com>
2026-07-30 15:33:03 +0500

    added python variable

---
+++ variable.py
@@ -0,0 +1,65 @@
+"""
+In python a variable is created, when you assign a value to it
+Python has no command for declaring a variable
+"""
+
+# Now we assign False value to y variable
+x = "logged in"
+y = "False"
+
+z = "Try again"
+
+# if 50 > 15:
+#     print(x)      
+# print(y)
+
+
+# Use case of variable
+if 'email is wrong':
+    print(z)
+
+if 'first name is wrong':
+    print(z)
+
+if "password is wrong":
+    print(z)
+
+if "phone number is wrong":
+    print(z)
+
+if "email and password correct":
+    print(x)
+
+if "first name is correct and email and password is also correct":
+    print(x)
+
+t = 48500
+r = 90700
+
+if t > r:
+    print('T is greater than r')
+
+if r > t:
+    print("r is greater than t")
+
+
+
+
+A =" she is wrong"
+E = "he is intelligent"
+
+if "she is true":
+    print(A)
+if "this statment is true":
+    print(E)
+
+
+W = 200
+S = 10
+
+if W > S:
+    print("w is greater than s")
+
+
+
+
