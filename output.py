6e1c928b18906ab3b55701413c4e599483159b0c
konainamber-01-arch <konainamber01@gmail.com>
2026-07-30 16:18:04 +0500

    added mix data type in python

---
+++ output.py
@@ -0,0 +1,17 @@
+# print("Good Ho gya")
+# print("My name is konain amber")
+# print("my father name is muhammad yameen")
+# print("this is my car")
+
+# printing str data type
+print("My name is Konain") 
+
+# printing int data type
+print(2548)
+
+# printing float data type
+print(2.4532)
+
+
+# printing mix data types
+print("hello worl" ,2345,3.4567, "world" )
